f81bdf8ac84054701b90beae5947ce60378b6c96
Dave Sproson <dave.sproson@ncas.ac.uk>
2018-09-17 16:32:38 +0100

    Added metres <-> feet conversions.

--- ppodd/utils/conversions.py
+++ ppodd/utils/conversions.py
@@ -3,3 +3,9 @@ def kelvin_to_celsius(myvar):
 
 def celsius_to_kelvin(myvar):
     return myvar + 273.15
+
+def metres_to_feet(metres):
+    return metres / 0.3048
+
+def feet_to_metres(feet):
+    return feet * 0.3048
