1458efd9a04c60ae9bd810ff3904efb6d9e9ee1a
Dave Sproson <dave.sproson@ncas.ac.uk>
2018-09-18 12:36:41 +0100

    Complete rehash of netCDF writer.

--- ppodd/writers/writers.py
+++ ppodd/writers/writers.py
@@ -1,5 +1,7 @@
 import abc
+import datetime
 
+import numpy as np
 import pandas as pd
 
 from netCDF4 import Dataset
@@ -19,107 +21,115 @@ class DecadesWriter(abc.ABC):
 
 class NetCDFWriter(DecadesWriter):
 
-    def _create_file(self, filename, indicies):
-        self.nc = Dataset(filename, 'w')
+    _FillValue = -9999
 
-        self.nc.createDimension('Time', None)
-        sps_vars = {}
-        for sps in indicies:
-            if sps == 1:
+    def __init__(self, *args, **kwargs):
+        self.output_freqs = []
+        self.start_time = None
+        self.end_time = None
+        self.nc = None
+        self.sps_vars = {}
+
+        super().__init__(*args, **kwargs)
+
+        self._get_time_bounds()
+        self._get_output_freqs()
+
+    def _get_output_freqs(self):
+        """Get all of the required output frequencies"""
+        output_freqs = []
+        for var in self.dataset.outputs:
+            if var.frequency not in output_freqs:
+                output_freqs.append(var.frequency)
+        self.output_freqs = output_freqs
+
+
+    def _get_time_bounds(self):
+        """Get the earliest and latest times of all output variables"""
+        start_time = datetime.datetime(2999, 1, 1)
+        end_time = datetime.datetime(1900, 1, 1)
+
+        for var in self.dataset.outputs:
+            if var.data.index[0] < start_time:
+                start_time = var.data.index[0]
+            if var.data.index[-1] > end_time:
+                end_time = var.data.index[-1]
+
+        start_time = start_time.replace(microsecond=0)
+
+        self.start_time = start_time
+        self.end_time = end_time
+
+    def _write_var(self, nc, var):
+        _var_name = var.name
+        _flag_name = '{}_FLAG'.format(var.name)
+
+        _freq = pd_freq[var.frequency]
+
+        if 60 % var.frequency == 0:
+            _end = self.end_time
+        else:
+            _end = self.end_time + datetime.timedelta(seconds=1/var.frequency)
+
+        _index = pd.DatetimeIndex(
+            start=self.start_time,
+            end=_end,
+            freq=_freq
+        )
+
+        _data = var.data.reindex(_index)
+        _flag = var.flag.reindex(_index)
+        _flag.loc[~np.isfinite(_flag)] = 3
+
+        print('Writing: {}'.format(var.long_name))
+
+        _shape = (int(len(_data) / var.frequency), var.frequency)
+
+        if var.frequency != 1:
+            nc[_var_name][:] = _data.values.reshape(_shape)
+            nc[_flag_name][:] = _flag.values.reshape(_shape)
+        else:
+            nc[_var_name][:] = _data.values
+            nc[_flag_name][:] = _flag.values
+
+        nc[_var_name].units = var.units
+        nc[_var_name].long_name = var.long_name
+        nc[_var_name].frequency = np.int32(var.frequency)
+        if var.standard_name is not None:
+            nc[_var_name].standard_name = var.standard_name
+
+    def _init_nc_file(self, nc):
+        nc.createDimension('Time', None)
+        for freq in self.output_freqs:
+            if freq == 1:
                 continue
-            sps_name = 'sps{0:02d}'.format(sps)
-            sps_vars[sps] = self.nc.createDimension(sps_name, sps)
+            sps_name = 'sps{0:02d}'.format(freq)
+            nc.createDimension(sps_name, freq)
 
-        for output in self.dataset.outputs:
-            print('writing {}'.format(output.long_name))
-            for _var in output:
-                if 'FLAG' in _var:
-                    is_flag = True
-                    _datatype = 'b'
-                    _fill_value = -1
-                else:
-                    is_flag = False
-                    _datatype = 'f'
-                    _fill_value = -9999
+        nc.createVariable('Time', int, ('Time',))
+        nc['Time'][:] = list(pd.DatetimeIndex(
+            start=self.start_time, end=self.end_time, freq='1s'
+        ).astype(int) / 1e9)
+        nc['Time'].units = 'seconds since 1970-01-01'
 
-                if output.frequency == 1:
-                    _dims = ('Time',)
-                else:
-                    _dims = ('Time', 'sps{0:02d}'.format(output.frequency))
-
-                ncvar = self.nc.createVariable(
-                    _var, _datatype, _dims, zlib=True, fill_value=_fill_value,
-                    complevel=9
+        for var in self.dataset.outputs:
+            if var.frequency == 1:
+                nc.createVariable(var.name, float, ('Time',))
+                nc.createVariable('{}_FLAG'.format(var.name), np.int8, ('Time',))
+            else:
+                nc.createVariable(
+                    var.name,
+                    float,
+                    ('Time', 'sps{0:02d}'.format(var.frequency))
                 )
-
-                if not is_flag:
-                    setattr(ncvar, 'long_name', output.long_name)
-                    if output.standard_name:
-                        setattr(ncvar, 'standard_name', output.standard_name)
-                    setattr(ncvar, 'units', output.units)
-                    setattr(ncvar, 'frequency', int(output.frequency))
-                else:
-                    setattr(ncvar, 'long_name', 'Flag for {}'.format(
-                            output.long_name))
-                    setattr(ncvar, 'units', '1')
-                    setattr(ncvar, 'frequency', int(output.frequency))
-
-                data = output[_var].reindex(
-                    indicies[output.frequency]
-                ).interpolate(limit=1)
-
-                if 'FLAG' in _var:
-                    data.fillna(3, inplace=True)
-                else:
-                    data.fillna(-9999, inplace=True)
-
-                ncvar[:] = data.values.reshape(
-                    (len(indicies[1]), output.frequency)
+                nc.createVariable(
+                    '{}_FLAG'.format(var.name),
+                    np.int8,
+                    ('Time', 'sps{0:02d}'.format(var.frequency))
                 )
-
-        time_var = self.nc.createVariable('Time', int, ('Time',))
-        secs = (indicies[1] - pd.to_datetime(indicies[1].date)).total_seconds()
-        time_var[:] = secs
-
-        self.nc.close()
 
     def write(self, filename):
-
-        start_index = None
-        end_index = None
-        freqs = []
-
-        for output in self.dataset.outputs:
-            if start_index is None or output.index[0] < start_index:
-                start_index = output.index[0]
-
-            if end_index is None or output.index[-1] > end_index:
-                end_index = output.index[-1]
-
-            if output.frequency not in freqs:
-                freqs.append(output.frequency)
-
-        print('start_index: {}'.format(start_index))
-        print('end_index: {}'.format(end_index))
-        print('freqs: {}'.format(freqs))
-
-        output_index = {}
-
-        for freq in freqs:
-            output_index[freq] = pd.DatetimeIndex(
-                start=start_index,
-                end=end_index,
-                freq=pd_freq[freq]
-            )
-
-            _mod = len(output_index[freq]) % freq
-            if _mod:
-                output_index[freq] = output_index[freq][:-_mod]
-            if freq == 1:
-                output_index[1] = output_index[1][:-1]
-
-        try:
-            self._create_file(filename, output_index)
-        except Exception:
-            self.nc.close()
-            raise
+        with Dataset(filename, 'w') as nc:
+            self._init_nc_file(nc)
+            for var in self.dataset.outputs:
+                self._write_var(nc, var)
