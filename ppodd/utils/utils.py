819f0656f0676b4032f576c1e8346b788d00e6c8
Dave Sproson <dave.sproson@ncas.ac.uk>
2018-09-20 10:31:01 +0100

    Added get_range_flag() to utils.

--- ppodd/utils/utils.py
+++ ppodd/utils/utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 pd_freq = {
     1: '1S',
     2: '500L',
@@ -11,3 +13,28 @@ pd_freq = {
     128: '7812500N',
     256: '3906250N'
 }
+
+def get_range_flag(var, limits, flag_val=2):
+    """
+    Get a flag variable which flags when a variable is outside a specified
+    range.
+
+      Args:
+          var: the variable on which to base the flagging, np.array-like.
+          limits: a 2-element array specifying valid-min and valid-max.
+
+      Kwargs:
+          flag_val: the value of the flag when var is outside limits. Default
+                    2.
+
+      Returns:
+          flag: a flag [np.array] of same dimensions as var, with value 0
+                wherever var in inside limits, and flag_val where are is
+                outside limits.
+    """
+    flag = np.zeros_like(var)
+
+    flag[flag < limits[0]] = flag_val
+    flag[flag > limits[1]] = flag_val
+
+    return flag
