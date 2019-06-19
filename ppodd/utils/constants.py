b5e43e1dfc2d752a2a5ddb76f6d59ffe48190dc3
Dave Sproson <dave.sproson@ncas.ac.uk>
2019-06-19 19:42:27 +0100

    Added the Stefan-Boltzmann constant to the constants package. Required by pyrgeometer processing (f0391720bc730e849).

--- ppodd/utils/constants.py
+++ ppodd/utils/constants.py
@@ -12,6 +12,9 @@ ICAO_STD_TEMP = 288.15
 # ICAO Standard pressure at surface (hPa)
 ICAO_STD_PRESS = 1013.25
 
+# Stefan-Boltzmann constant
+STEF_BOLTZ = 5.67e-8
+
 # Max and min of the Radar Altimeter
 # Rad Alt is 16 bit signed int, with least significant bit resolution of
 # 0.25 ft, so max valid value is (((2**16) / 2) - 1) / 4
