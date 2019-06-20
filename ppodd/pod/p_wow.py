a9725e1322d630cf56bcbc6fe4b7123aa3ad2abd
Dave Sproson <dave.sproson@ncas.ac.uk>
2019-06-20 13:26:58 +0100

    Minor changes to WOW.     - Remove unit declaration.     - Back and forward fill nans.

--- ppodd/pod/p_wow.py
+++ ppodd/pod/p_wow.py
@@ -4,25 +4,29 @@ from ..decades import DecadesVariable
 from .base import PPBase
 
 
-class RioWeightOnWheels(PPBase):
+class WeightOnWheels(PPBase):
 
     inputs = ['PRTAFT_wow_flag']
 
     def declare_outputs(self):
         self.declare(
             'WOW_IND',
-            units='-',
             frequency=1,
             long_name='Weight on wheels indicator',
             standard_name=None
         )
 
     def process(self):
+        """
+        Processing entry point.
+        """
 
         self.get_dataframe()
         wow = pd.DataFrame()
 
         wow['WOW_IND'] = self.d['PRTAFT_wow_flag'].asfreq('1S')
+        wow['WOW_IND'].fillna(method='bfill', inplace=True)
+        wow['WOW_IND'].fillna(method='ffill', inplace=True)
 
         self.add_output(
             DecadesVariable(
