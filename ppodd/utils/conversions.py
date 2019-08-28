c1cc2a151efafd743f72f02b93f864c4fbaa1971
Dave Sproson <dave.sproson@ncas.ac.uk>
2019-08-28 13:15:22 +0100

    Added u, v to speed, direction util function in conversions.py.

--- ppodd/utils/conversions.py
+++ ppodd/utils/conversions.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def kelvin_to_celsius(myvar):
     return myvar - 273.15
 
@@ -9,3 +11,8 @@ def metres_to_feet(metres):
 
 def feet_to_metres(feet):
     return feet * 0.3048
+
+def uv_to_spddir(u, v):
+    _spd = (u**2 + v**2) ** .5
+    _dir = np.arctan2(u/_spd, v/_spd) * 180 / np.pi
+    return _spd, _dir
