93743f253f286de386e96513ea178a853d7ae849
Dave Sproson <dave.sproson@ncas.ac.uk>
2019-09-09 16:34:47 +0100

    Minor changes to TECO O3 module.

--- ppodd/pod/p_tei_ozone.py
+++ ppodd/pod/p_tei_ozone.py
@@ -27,29 +27,33 @@ class TeiOzone(PPBase):
 
     def flag(self):
         FLOW_THRESHOLD = 0.5
+        CONC_THRESHOLD = -10
+        FLAG_AFTER_TO = 20
 
         d = self.d
 
+        d['TEIOZO_flag'].fillna(value='', inplace=True)
         flag =  np.array([i.lower() for i in d['TEIOZO_flag']])
+        flag[flag == ''] = 3
 
         if '1c100000' in flag:
             flag = flag != '1c100000'
 
         if '0c100000' in flag:
-            flag = flag!='0c100000'
+            flag = flag != '0c100000'
+
+
+        flag[d['TEIOZO_conc'] < CONC_THRESHOLD] = 2
+        flag[d['TEIOZO_FlowA'] < FLOW_THRESHOLD] = 3
+        flag[d['TEIOZO_FlowB'] < FLOW_THRESHOLD] = 3
+        flag[d['WOW_IND'] != 0] = 3
+
+        to_ix = int(np.where(d['WOW_IND'].diff() == -1)[0][0])
+        flag[to_ix:to_ix + FLAG_AFTER_TO + 1] = 3
 
         flag = flag.astype('int8')
 
         d['FLAG'] = flag
-
-        d.loc[d['TEIOZO_conc'] < -10, 'FLAG'] = 2
-        d.loc[d['TEIOZO_FlowA'] < FLOW_THRESHOLD, 'FLAG'] = 3
-        d.loc[d['TEIOZO_FlowB'] < FLOW_THRESHOLD, 'FLAG'] = 3
-        d.loc[d['WOW_IND'] != 0, 'FLAG'] = 3
-
-        to_ix = int(np.where(d['WOW_IND'][:-1] - d['WOW_IND'][1:] == 1)[0])
-        d.iloc[to_ix:to_ix + 20, 'FLAG'] = 3
-
 
     def process(self):
         self.get_dataframe()
