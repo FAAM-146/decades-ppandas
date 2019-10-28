04460e26fc7d527388a40a533a50bd5ab77ce46e
Dave Sproson <dave.sproson@ncas.ac.uk>
2019-10-28 18:46:35 +0000

    Modified p_cabin_pressure.py for class based flagging

--- ppodd/pod/p_cabin_pressure.py
+++ ppodd/pod/p_cabin_pressure.py
@@ -36,8 +36,8 @@ class CabinPressure(PPBase):
         valid_max = 1050
 
         self.d['BOUNDS_FLAG'] = 0
-        self.d.loc[self.d['CAB_PRES'] < valid_min, 'BOUNDS_FLAG'] = 2
-        self.d.loc[self.d['CAB_PRES'] > valid_max, 'BOUNDS_FLAG'] = 2
+        self.d.loc[self.d['CAB_PRES'] < valid_min, 'BOUNDS_FLAG'] = 1
+        self.d.loc[self.d['CAB_PRES'] > valid_max, 'BOUNDS_FLAG'] = 1
 
     def process(self):
         """
@@ -50,6 +50,11 @@ class CabinPressure(PPBase):
 
         self.bounds_flag()
 
-        s9 = DecadesVariable(self.d['CAB_PRES'])
-        s9.add_flag(self.d['BOUNDS_FLAG'])
-        self.add_output(s9)
+        cab_press = DecadesVariable(self.d['CAB_PRES'])
+
+        cab_press.flag.add_meaning(0, 'data good')
+        cab_press.flag.add_meaning(1, 'pressure out of range')
+
+        cab_press.flag.add_flag(self.d['BOUNDS_FLAG'])
+
+        self.add_output(cab_press)
