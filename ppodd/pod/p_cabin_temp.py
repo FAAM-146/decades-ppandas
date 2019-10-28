43e46051913c0d2158ab590834cc3aacbba06c40
Dave Sproson <dave.sproson@ncas.ac.uk>
2019-10-28 18:46:46 +0000

    Modified p_cabin_temp.py for class based flagging

--- ppodd/pod/p_cabin_temp.py
+++ ppodd/pod/p_cabin_temp.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from .base import PPBase
 from ..decades import DecadesVariable
+from ..decades import flags
 
 
 class CabinTemp(PPBase):
@@ -31,6 +32,14 @@ class CabinTemp(PPBase):
 
         # Temperature is just a polynomial cal from CORCON_cabin_t
         d['CAB_TEMP'] = np.polyval(_cals, d.CORCON_cabin_t)
-        d['CAB_TEMP_FLAG'] = 2
+        d['CAB_TEMP_FLAG'] = 1
 
-        self.add_output(DecadesVariable(d[['CAB_TEMP', 'CAB_TEMP_FLAG']]))
+        temp = DecadesVariable(d['CAB_TEMP'], name='CAB_TEMP')
+
+        temp.flag.add_meaning(0, flags.DATA_GOOD)
+        temp.flag.add_meaning(1, 'sensor uncalibrated')
+        temp.flag.add_meaning(2, flags.DATA_MISSING)
+
+        temp.flag.add_flag(d['CAB_TEMP_FLAG'])
+
+        self.add_output(temp)
