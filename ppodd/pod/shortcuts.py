01644e6f84ca63093bae2bd03711a9a232922464
Dave Sproson <dave.sproson@ncas.ac.uk>
2019-11-20 14:57:48 +0000

    Add np.linspace to pp module import shortcuts.

--- ppodd/pod/shortcuts.py
+++ ppodd/pod/shortcuts.py
@@ -1,5 +1,6 @@
 from numpy import (
-    concatenate as _c, ones as _o, zeros as _z, arange as _a
+    concatenate as _c, ones as _o, zeros as _z, arange as _a,
+    linspace as _l
 )
 
-__all__ = ('_a', '_c', '_o', '_z')
+__all__ = ('_a', '_c', '_o', '_z', '_l')
