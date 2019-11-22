2d8e4e762c04e1ef0d991cbcf3e63de717ca4fb8
Dave Sproson <dave.sproson@ncas.ac.uk>
2019-11-22 18:46:18 +0000

    Added some version stuff to ppodd.

---
+++ ppodd/__init__.py
@@ -0,0 +1,56 @@
+import contextlib
+import os
+import inspect
+import subprocess
+
+import ppodd
+
+__version__ = '0.1.0'
+
+@contextlib.contextmanager
+def flipdir(path):
+    """
+    Provides a quick-and-dirty context manager to quickly flip to a specified
+    directory, do some stuff, and change back.
+
+    Args:
+        path: the path to change to.
+    """
+    cwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield path
+    except Exception:
+        raise
+    finally:
+        os.chdir(cwd)
+
+def githash():
+    """
+    Return the hash of the current git commit assuming that
+        a) the code is sitting in a git repo;
+        b) the git cli is available on the system.
+
+    Returns:
+        output: the hash of the current git commit, or None if this cannot be
+                obtained.
+    """
+    path = os.path.dirname(inspect.getfile(ppodd))
+
+    with flipdir(path) as _path:
+        try:
+            with open(os.devnull) as devnull:
+                output = subprocess.check_output(
+                    ['git', 'log', '-1', '--oneline'],
+                    stderr=devnull
+                )
+        except subprocess.CalledProcessError:
+            output = None
+
+    if output:
+        try:
+            output = output.decode().split()[0]
+        except (AttributeError, IndexError):
+            return None
+
+    return output
