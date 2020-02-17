FAAM-146/decades-ppandas

commit 3ff0c5cafe6fc242eb75a830cca91eac2a1b6c2c

Init pp / qa modules after loading data into dataset, to make constants available a declaration time.

ppodd/decades/decades.py
@@ -10,6 +10,8 @@ import numpy as np
 import pandas as pd
 
 import ppodd
+import ppodd.pod
+import ppodd.qa
 
 from .backends import PandasInMemoryBackend
 from .globals import GlobalsCollection
@@ -138,14 +140,6 @@ class DecadesDataset(object):
         self._landing_time = None
         self._decache = False
         self._backend = backend()
-
-        import ppodd.pod
-        import ppodd.qa
-
-        self.qa_modules = [qa(self) for qa in ppodd.qa.qa_modules]
-        self.pp_modules = collections.deque(
-            [pp(self) for pp in ppodd.pod.pp_modules]
-        )
 
         self._default_globals()
 
@@ -451,6 +445,12 @@ class DecadesDataset(object):
 
         self.readers = None
         gc.collect()
+
+        self.qa_modules = [qa(self) for qa in ppodd.qa.qa_modules]
+        self.pp_modules = collections.deque(
+            [pp(self) for pp in ppodd.pod.pp_modules]
+        )
+
         self._interpolate_globals()
 
     @property

commit c87e13bd8690207f6382b214e2063833c2553c5d

Fix import bug introduced in 3ff0c5cafe.

ppodd/decades/decades.py
@@ -10,8 +10,6 @@ import numpy as np
 import pandas as pd
 
 import ppodd
-import ppodd.pod
-import ppodd.qa
 
 from .backends import PandasInMemoryBackend
 from .globals import GlobalsCollection
@@ -32,7 +30,8 @@ class DecadesVariable(object):
 
     NC_ATTRS = [
         'long_name', 'frequency', 'standard_name', 'units',
-        '_FillValue', 'valid_min', 'valid_max', 'comment'
+        '_FillValue', 'valid_min', 'valid_max', 'comment', 'sensor_type',
+        'sensor_serial', 'instrument_serial'
     ]
 
     def __init__(self, *args, **kwargs):
@@ -438,6 +437,8 @@ class DecadesDataset(object):
         Load all of the data from files associated with readers in this
         dataset.
         """
+        import ppodd.pod
+        import ppodd.qa
 
         for reader in self.readers:
             reader.read()