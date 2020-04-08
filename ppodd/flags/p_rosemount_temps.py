53cb1f3b50430361487b5d52c705d999e4f0ac7c
Dave Sproson <dave.sproson@ncas.ac.uk>
2020-04-08 14:29:08 +0100

    Added a flag to the Rosemount temperatures to indicate the presence of cloud.

--- ppodd/flags/p_rosemount_temps.py
+++ ppodd/flags/p_rosemount_temps.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 from .base import FlaggingBase
 
-class RosemountTempFlag(FlaggingBase):
+
+class RosemountTempDeltaFlag(FlaggingBase):
     inputs = ['TAT_DI_R', 'TAT_ND_R']
 
     def flag(self):
@@ -20,4 +21,27 @@ class RosemountTempFlag(FlaggingBase):
         for var in ('TAT_DI_R', 'TAT_ND_R'):
             self.dataset[var].flag.add_mask(
                 mask, 'discrepancy threshold exceeded'
+            )
+
+
+class RosemountTempCloudFlag(FlaggingBase):
+    inputs = ['NV_CLEAR_AIR_MASK']
+
+    def flag(self):
+        for var in ('TAT_DI_R', 'TAT_ND_R'):
+            try:
+                index = self.dataset[var].index
+            except (KeyError, AttributeError):
+                continue
+
+            mask = self.dataset['NV_CLEAR_AIR_MASK'].data == 0
+            nv_start = self.dataset['NV_CLEAR_AIR_MASK'].data.index[0]
+            nv_end = self.dataset['NV_CLEAR_AIR_MASK'].data.index[-1]
+            mask = mask.reindex(index)
+            mask.loc[(mask.index <= nv_start) | (mask.index >= nv_end)] = 0
+            mask.fillna(method='ffill', inplace=True)
+            mask.fillna(method='bfill', inplace=True)
+
+            self.dataset[var].flag.add_mask(
+                mask, 'in cloud'
             )
