5ff0847a8b0167510033d18733f46abadc3c2273
Dave Sproson <daspr@faam.ac.uk>
2020-06-16 16:52:36 +0100

    Added SLR identification to utils.

--- ppodd/utils/utils.py
+++ ppodd/utils/utils.py
@@ -142,6 +142,69 @@ def try_to_call(call, compliance=False):
         return call
 
 
+def slrs(wow, ps, roll, min_length=120, max_length=None, roll_lim=3,
+         ps_lim=2, roll_mean=5, freq=1):
+
+    def _add_slrs(slrs, group):
+        _gdf = group[1]
+        if max_length is None:
+            slrs.append(_gdf)
+            return
+        num_groups = len(_gdf) // max_length
+        last_index = -(len(_gdf) % max_length)
+        _gdfs = np.split(_gdf.iloc[:last_index], num_groups)
+        slrs += _gdfs
+
+        rem = _gdf.iloc[last_index:]
+        if len(rem) >= min_length:
+            slrs.append(rem)
+
+    window_size = min_length
+    slrs = []
+
+    if max_length is not None and max_length < min_length:
+        raise ValueError('max_length must be >= min_length')
+
+    _df = pd.DataFrame()
+    _df['WOW_IND'] = wow
+    _df['PS_RVSM'] = ps
+    _df['ROLL_GIN'] = roll
+    _df = _df.asfreq('1S')
+
+    # Drop an data on the ground
+    _df.loc[_df.WOW_IND == 1] = np.nan
+    _df.dropna(inplace=True)
+
+    # Check the variance of the static pressure is sufficiently small
+    _df['PS_C'] = _df.PS_RVSM.rolling(
+        window_size, center=True
+    ).std() < ps_lim
+
+    # Check that the range of the GIN roll is inside acceptable limits
+    _df['ROLL_C'] = _df.ROLL_GIN.rolling(roll_mean).mean().rolling(
+        window_size, center=True
+    ).apply(np.ptp, raw=True) < roll_lim
+
+    # Identify discontiguous regions which pass the selection criteria
+    # and group them
+    _df['_SLR'] = (_df['PS_C'] & _df['ROLL_C']).astype(int)
+    _df['_SLRCNT'] = (_df._SLR.diff(1) != 0).astype('int').cumsum()
+    groups = _df.groupby(_df._SLRCNT)
+
+    slrs = []
+    for group in groups:
+        _df = group[1]
+        if _df._SLR.mean() == 0:
+            continue
+        if len(_df) < window_size:
+            continue
+
+        # Add slrs to list, splitting if required
+        _add_slrs(slrs, group)
+
+    return [i.asfreq('{0:0.0f}N'.format(1e9/freq)).index for i in slrs]
+
+
 class Either(object):
 
     def __init__(self, *args, name=None):
