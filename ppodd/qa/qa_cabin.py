82c18ebc6745fc0269d33e523a450dfc1cd0ced3
Dave Sproson <daspr@faam.ac.uk>
2020-11-04 16:09:25 +0000

    Added qa plot for in-cabin t/p.

---
+++ ppodd/qa/qa_cabin.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+from .base import QAMod, QAFigure
+
+class Cabin(QAMod):
+    inputs = [
+        'CAB_TEMP',
+        'CAB_PRES',
+        'PS_RVSM'
+    ]
+
+    def make_temp_timeseries(self, fig):
+        ax = fig.timeseries_axes([.1, .55, .8, .35], labelx=False)
+
+        ts = fig.filter_in_flight(self.dataset['CAB_TEMP'].data)
+        ax.plot(ts, label='Cabin Temperature')
+        ax.set_ylabel('Temperature ($^\circ$C)')
+        ax.legend(fontsize=6)
+
+    def make_pressure_timeseries(self, fig):
+        ax1, ax2 = fig.timeseries_axes([.1, .15, .8, .35], twinx=True)
+        cab_ts = fig.filter_in_flight(self.dataset['CAB_PRES'].data)
+        ps_ts = fig.filter_in_flight(self.dataset['PS_RVSM'].data)
+        ax1.plot(cab_ts, label='Cabin pressure')
+        ax2.plot(ps_ts, 'tab:orange', label='Ambient pressure')
+        ax1.set_ylabel('Cabin pressure (hPa)')
+        ax2.set_ylabel('Ambient pressure (hPa)')
+        ax1.legend(loc='lower left', fontsize=6)
+        ax2.legend(loc='lower right', fontsize=6)
+
+    def run(self):
+        with QAFigure(self.dataset, 'Cabin', landscape=True) as fig:
+            self.make_temp_timeseries(fig)
+            self.make_pressure_timeseries(fig)
