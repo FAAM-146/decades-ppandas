1105d24f3aecabd7874edda1a57a50f8364e1851
Dave Sproson <daspr@faam.ac.uk>
2020-11-26 11:27:19 +0000

    Added initial stab at defining variable attributes.

--- ppodd/standard/__init__.py
+++ ppodd/standard/__init__.py
@@ -2,6 +2,10 @@ import os
 import yaml
 
 GLOBALS_FILE = os.path.join(os.path.dirname(__file__), 'globals.yaml')
+ATTRS_FILE = os.path.join(os.path.dirname(__file__), 'variable_attrs.yaml')
 
 with open(GLOBALS_FILE, 'r') as f:
     faam_globals = yaml.load(f, Loader=yaml.CLoader)
+
+with open(ATTRS_FILE, 'r') as f:
+    faam_attrs = yaml.load(f, Loader=yaml.CLoader)
