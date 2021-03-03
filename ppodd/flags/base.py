038117daf265c7e0fbfc6d04959c619f4574a08d
Dave Sproson <daspr@faam.ac.uk>
2021-03-03 11:56:44 +0000

    Tidying and documentation.

--- ppodd/flags/base.py
+++ ppodd/flags/base.py
@@ -1,12 +1,36 @@
+"""
+Provides an abstract base class for flagging modules. Flagging modules run
+after the main processing modules allowing modification to data quality flags
+using data which may not be available at the time processing modules are run.
+"""
+
 import abc
 
 class FlaggingBase(abc.ABC):
+    """
+    An abstract base class from which flagging modules should inherit.
+    """
+
+    # Names of required input variables
     inputs = []
 
     def __init__(self, dataset):
+        """
+        Initialize an instance.
+
+        Args:
+            dataset: a DecadesDataset from which the processing is being run.
+        """
         self.dataset = dataset
 
     def ready(self):
+        """
+        Decide if the module is ready to run. It can be run if all of its
+        inputs are available in the parent dataset.
+
+        Returns:
+            True if the module is ready to run, False otherwise.
+        """
         for _input in self.inputs:
             if _input not in self.dataset.inputs + self.dataset.outputs:
                 return False
--- ppodd/flags/p_rosemount_temps.py
+++ ppodd/flags/p_rosemount_temps.py
@@ -1,41 +1,75 @@
+"""
+This module provides flagging modules for the Rosemount temperature probes,
+providing data quality information which can not be inferred during processing
+of the temperatures.
+"""
+
 import numpy as np
 import pandas as pd
 
-from .base import FlaggingBase
+from ppodd.flags.base import FlaggingBase
 
+TEMPERATURE_THRESHOLD = 1.
+TEMPERATURE_VARIABLES = ('TAT_DI_R', 'TAT_ND_R')
+MASKED = 1
+UNMASKED = 0
 
 class RosemountTempDeltaFlag(FlaggingBase):
-    inputs = ['TAT_DI_R', 'TAT_ND_R']
+    """
+    The temperature delta flag adds a flag to the rosemount temperatures if the
+    two temperatures disagree by more than a given absolute value. This is
+    given by the module constant TEMPERATURE_THRESHOLD.
+    """
+
+    inputs = list(TEMPERATURE_VARIABLES)
 
     def flag(self):
+        """
+        Entry point for the flagging module.
+        """
+
+        # Get the absolute difference between the deiced and non-deiced
+        # temperatures
         _diff = np.abs(
             self.dataset['TAT_DI_R'].array - self.dataset['TAT_ND_R'].array
         )
 
+        # Create a mask for the flag
         mask = pd.Series(
             np.zeros_like(self.dataset['TAT_DI_R'].array),
             index=self.dataset['TAT_DI_R'].index
         )
-        mask.loc[_diff > 1.] = 1
+        mask.loc[_diff > TEMPERATURE_THRESHOLD] = MASKED
 
-        for var in ('TAT_DI_R', 'TAT_ND_R'):
+        # For each of the temperature add the threshold mask
+        for var in TEMPERATURE_VARIABLES:
             self.dataset[var].flag.add_mask(
                 mask, 'discrepancy threshold exceeded'
             )
 
 
 class RosemountTempCloudFlag(FlaggingBase):
+    """
+    The cloud flags adds a flag to all temperature variables if the aircraft is
+    in cloud, as indicated by NV_CLEAR_AIR_MASK, derived from the Nevzerov
+    probe.
+    """
+
     inputs = ['NV_CLEAR_AIR_MASK']
 
     def flag(self):
-        for var in ('TAT_DI_R', 'TAT_ND_R'):
+        """
+        Entry point for the flagging module.
+        """
+
+        for var in TEMPERATURE_VARIABLES:
             try:
                 index = self.dataset[var].index
             except (KeyError, AttributeError):
                 continue
 
             clear_air_series = self.dataset['NV_CLEAR_AIR_MASK']()
-            mask = clear_air_series == 0
+            mask = clear_air_series == UNMASKED
             nv_start = clear_air_series.index[0]
             nv_end = clear_air_series.index[-1]
             mask = mask.reindex(index)
