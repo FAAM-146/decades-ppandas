ab527ee80678b450bb4a8b37e403b3ce79ac8468
Dave Sproson <daspr@faam.ac.uk>
2021-03-04 12:32:05 +0000

    Tidying p_cabin_pressure.

--- ppodd/pod/p_cabin_pressure.py
+++ ppodd/pod/p_cabin_pressure.py
@@ -1,3 +1,8 @@
+"""
+This module provides a processing module which calculates cabin pressure, see
+class docstring for more info.
+"""
+
 import numpy as np
 
 from ..decades import DecadesVariable
@@ -22,6 +27,9 @@ class CabinPressure(PPBase):
 
     @staticmethod
     def test():
+        """
+        Return some dummy input data for testing.
+        """
         return {
             'CALCABP': ('const', [2.75, 3.3e-2, -2.5e-10]),
             'CORCON_cabin_p': (
@@ -58,18 +66,23 @@ class CabinPressure(PPBase):
         _cals = self.dataset['CALCABP'][::-1]
         self.get_dataframe()
 
+        # Polynomial conversion of dlu counts to pressure
         self.d['CAB_PRES'] = np.polyval(_cals, self.d['CORCON_cabin_p'])
 
+        # Create a simple flag based on a defined valid limit
         self.bounds_flag()
 
+        # Create the output variable
         cab_press = DecadesVariable(self.d['CAB_PRES'])
 
+        # Create the data quality flag meanings.
         cab_press.flag.add_meaning(0, 'data good', 'Data are considered valid')
         cab_press.flag.add_meaning(
             1, 'pressure out of range', ('Data are outside the valid range '
                                          f'[{VALID_MIN}, {VALID_MAX}] hPa')
         )
 
+        # Add the flag to the output
         cab_press.flag.add_flag(self.d['BOUNDS_FLAG'])
 
         self.add_output(cab_press)
