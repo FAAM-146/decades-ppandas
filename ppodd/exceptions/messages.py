fabe64a48fe78168d9a984b95ceaf1852befa877
Dave Sproson <daspr@faam.ac.uk>
2021-03-09 17:29:45 +0000

    Added an exceptions module, for custom exeption classes and common error messages.

---
+++ ppodd/exceptions/messages.py
@@ -0,0 +1,6 @@
+EM_CANNOT_INIT_MODULE = (
+    'Cannot initialise module {module_name} as one or more of the constants '
+    '({constants}) are not defined on the controlling Dataset. These are '
+    'required at initialization time. This operation cannot be deferred. '
+    'ensure these constants are set if this module is required.'
+)
