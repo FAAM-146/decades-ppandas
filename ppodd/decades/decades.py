986336e8f93887956e1c5c608c38b746100ca018
Dave Sproson <daspr@faam.ac.uk>
2021-03-09 11:18:21 +0000

    Add <Lazy> as a class to defer evaluation of dataset/variable attributes until later.

--- ppodd/decades/decades.py
+++ ppodd/decades/decades.py
@@ -35,6 +35,56 @@ from ppodd.utils import pd_freq, infer_freq
 from ppodd.writers import NetCDFWriter
 
 
+class Lazy(object):
+    """
+    A hacky deferral wrapper for assigning dataset constants to processing
+    module outputs potentially before they've actually been set on the dataset.
+    This works in this context as variable metadata are allowed to be either
+    literal or callable.
+    """
+
+    def __init__(self, parent):
+        """
+        Initialize a class instance.
+
+        Args:
+            parent: the object whos state we want to defer.
+        """
+        self.parent = parent
+
+    def __getitem__(self, item):
+        """
+        Implement [x], potentially deferred to a callable.
+
+        Args:
+            item: the item to get from the parent.
+
+        Returns:
+            either <item> got from parent, or a callable deferring this
+            operation.
+        """
+        try:
+            return self.parent[item]
+        except KeyError:
+            return lambda: self.parent[item]
+
+    def __getattr__(self, attr):
+        """
+        Implement .x, potentially deferred to a callable.
+
+        Args:
+            attr: the attribute to get from the parent.
+
+        Returns:
+            either <attr> from parent, or a callable deferring this
+            operation.
+        """
+        try:
+            return getattr(self.parent, attr)
+        except AttributeError:
+            return lambda: getattr(self.parent, attr)
+
+
 class DecadesFile(object):
     """
     A DecadesFile is just a wrapper around a filepath. Factored out for
@@ -137,7 +187,7 @@ class DecadesVariable(object):
         # don't have to have a 64-bit index associated with every variable.
         self.array = self._downcast(np.array(
             _df.reindex(
-                _index, tolerance=_freq, method='nearest', limit=1
+                _index, tolerance=0, method='nearest', limit=1
             ).values.flatten()
         ))
 
@@ -412,6 +462,7 @@ class DecadesDataset(object):
         """
 
         self._date = date
+        self.lazy = Lazy(self)
         self.readers = []
         self.definitions = []
         self.constants = {}
