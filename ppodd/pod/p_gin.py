d9725c7b0f93ad3332673a17ac92578af84b4cdd
Dave Sproson <daspr@faam.ac.uk>
2021-03-15 17:46:29 +0000

    Removed units from gin status.

--- ppodd/pod/p_gin.py
+++ ppodd/pod/p_gin.py
@@ -264,7 +264,7 @@ class Gin(PPBase):
         self.declare(
             'STATUS_GIN',
             frequency=32,
-            units=1,
+            units=None,
             long_name=f'Solution status from {gin_name}',
             standard_name=None,
             instrument_manufacturer=gin_manufacturer,
