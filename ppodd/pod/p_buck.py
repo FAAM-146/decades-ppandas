FAAM-146/decades-ppandas

commit 76e6e7966f00684b2dcd4c0e8932cc773ec8e82b

Correct bug in buck enhancement factor calcs, noticed by HP.

ppodd/pod/p_buck.py
@@ -533,16 +533,15 @@ class BuckCR2(PPBase):
         """
 
         c = self.get_enhance_coeff(buck_mirror_ctl)
-        result = (np.exp(
-            (1.0 - vp_buck / (buck_pressure * 100))
-            * c[:, 0] + c[:, 1] * buck_mirror_t + c[:, 2] * buck_mirror_t**2
-            + c[:, 3] * buck_mirror_t**3
-        ) + ((buck_pressure * 100) / vp_buck - 1)
-            * np.exp(
-                c[:, 4] + c[:, 5] * buck_mirror_t + c[:, 6]
-                * buck_mirror_t**2 + c[:, 7]*buck_mirror_t**3
-            )
-        )
+        t = buck_mirror_t
+
+        alpha = c[:, 0] + (c[:, 1] * t) + (c[:, 2] * t**2) + (c[:, 3] * t**3)
+        beta = c[:, 4] + (c[:, 5] * t) + (c[:, 6] * t**2) + (c[:, 7] * t**3)
+
+        vp = vp_buck
+        p = buck_pressure * 100
+
+        result = np.exp(alpha * (1 - (vp / p)) + beta * ((p / vp) - 1))
 
         return result
 

commit dda6973226b1dff5bd2fe7be25993184bdb3cd67

Fix error caused by misreading equation in 76e6e7966f.

ppodd/pod/p_buck.py
@@ -536,12 +536,15 @@ class BuckCR2(PPBase):
         t = buck_mirror_t
 
         alpha = c[:, 0] + (c[:, 1] * t) + (c[:, 2] * t**2) + (c[:, 3] * t**3)
-        beta = c[:, 4] + (c[:, 5] * t) + (c[:, 6] * t**2) + (c[:, 7] * t**3)
+        ln_beta = c[:, 4] + (c[:, 5] * t) + (c[:, 6] * t**2) + (c[:, 7] * t**3)
+        beta = np.exp(ln_beta)
 
         vp = vp_buck
         p = buck_pressure * 100
 
-        result = np.exp(alpha * (1 - (vp / p)) + beta * ((p / vp) - 1))
+        result = np.exp(
+            (alpha * (1 - (vp / p))) + (beta * ((p / vp) - 1))
+        )
 
         return result
 