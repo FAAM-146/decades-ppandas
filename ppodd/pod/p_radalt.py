86bc4caf8e2e2a6a44167aa7548ce3a2f2dd5681
Dave Sproson <daspr@faam.ac.uk>
2021-06-03 17:26:02 +0100

    Improved flagging for the radar altimeter.

--- ppodd/pod/p_radalt.py
+++ ppodd/pod/p_radalt.py
@@ -9,27 +9,28 @@ from ..decades import DecadesVariable, DecadesBitmaskFlag
 from ..decades import flags
 from ..utils.conversions import feet_to_metres
 from .base import PPBase, register_pp
-from .shortcuts import _l
+from .shortcuts import _l, _z
 
 RADALT_MIN = 10
-RADALT_MAX = 8100
+RADALT_MAX = feet_to_metres(5000)
 
 
 @register_pp('core')
 class RadAlt(PPBase):
     r"""
     Calculate the radar altitude, in metres. The radar altitude, from radalt2,
-    is read from the aircraft ARINC-429 data bus at the rear core console, at
-    a frequency of 2 Hz. The signal is received as a 16 bit signed int, with a
-    least significant bit resolution of 0.25 ft, giving a max valid value of
-    :math:`(((2^{16} / 2) - 1) / 4)`.
+    is read from the aircraft ARINC-429 data bus at the rear core console, and
+    recodred at a frequency of 2 Hz. The signal is received as a 16 bit signed
+    integer, with a least significant bit resolution of 0.25 ft, giving a max
+    valid value of :math:`(((2^{16} / 2) - 1) / 4)`.
 
-    Data are flagged when outside a slightly narrower range than the range of
-    the 16 bit signal.
+    Data are flagged when below 10 feet or above 5000 feet, which is the
+    nominal maximum range of the instrument.
     """
 
     inputs = [
-        'PRTAFT_rad_alt'    # Radar altitude (dlu)
+        'PRTAFT_rad_alt',    # Radar altitude (dlu)
+        'WOW_IND'            # Weight on wheels indicator (derived)
     ]
 
     @staticmethod
@@ -38,7 +39,8 @@ class RadAlt(PPBase):
         Provide dummy input data for testing.
         """
         return {
-            'PRTAFT_rad_alt': ('data', _l(0, 32000, 100), 2)
+            'PRTAFT_rad_alt': ('data', _l(0, 32000, 100), 2),
+            'WOW_IND': ('data', _z(100), 1)
         }
 
     def declare_outputs(self):
@@ -61,9 +63,13 @@ class RadAlt(PPBase):
         """
         d = self.d
         d['RANGE_FLAG'] = 0
+        d['WOW_FLAG'] = 0
 
         d.loc[d['HGT_RADR'] >= RADALT_MAX, 'RANGE_FLAG'] = 3
         d.loc[d['HGT_RADR'] <= RADALT_MIN, 'RANGE_FLAG'] = 3
+
+        wow = w['WOW_IND'].fillna(method='bfill').fillna(method='ffill')
+        d.loc[wow == 0, 'WOW_FLAG'] = 1
 
     def process(self):
         """
@@ -79,6 +85,14 @@ class RadAlt(PPBase):
         dv = DecadesVariable(d['HGT_RADR'], flag=DecadesBitmaskFlag)
         dv.flag.add_mask(
             d['RANGE_FLAG'], flags.OUT_RANGE,
-            f'RadAlt reading outside valid range ({RADALT_MIN}, {RADALT_MAX})'
+            (f'RadAlt reading outside valid range '
+             f'({RADALT_MIN:0.2f}, {RADALT_MAX:0.2f})')
         )
+
+        dv.flag.add_mask(
+            d['WOW_FLAG'], flags.WOW,
+            ('The aircraft is on the ground, as indicated by the weight '
+             'on wheels flag.')
+        )
+
         self.add_output(dv)
