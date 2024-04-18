e5133bc87322a35e700170434194bc84b0d5887f
Dave Sproson <dave.sproson@faam.ac.uk>
2024-04-18 10:37:19 +0100

    Update docs to new template.

--- ppodd/docs/data/conf.py
+++ ppodd/docs/data/conf.py
@@ -10,15 +10,13 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
-import ppodd
 import datetime
+import ppodd
+
 
 def setup(app):
-    app.add_css_file('mods.css')
+    app.add_css_file('faam.css')
 
 # -- Project information -----------------------------------------------------
 
@@ -26,7 +24,7 @@ project = 'FAAM Core Data Product'
 copyright = f'{datetime.datetime.now().year}, FAAM'
 author = 'FAAM'
 release = f'{ppodd.version()} ({ppodd.githash()})'
-
+version = release
 
 # -- General configuration ---------------------------------------------------
 
@@ -34,8 +32,12 @@ release = f'{ppodd.version()} ({ppodd.githash()})'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme'
+    'sphinx_rtd_theme',   # Read the docs theme
+    'sphinx.ext.autodoc', # Auto doc code documentation
+    'sphinx.ext.napoleon' # Google style docstrings
 ]
+
+napoleon_include_init_with_doc = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -52,18 +54,60 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'base_rst']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['static']
-
-#latex_toplevel_sectioning = 'section'
-latex_elements = {
-  'extraclassoptions': 'openany,oneside'
+html_logo = "static/faam-small.png"
+html_theme_options = {
+    'logo_only': False,
+    'display_version': True,
 }
-latex_logo = '../static/faam.png'
 html_js_files = [
     'https://cdn.jsdelivr.net/npm/iframe-resizer@4.3.2/js/iframeResizer.contentWindow.min.js',
 ]
 
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+latex_elements = {
+    'papersize': 'a4paper',
+    'extraclassoptions': 'openany,oneside',
+    'preamble': r'''
+        \definecolor{FAAMDarkBlue}{HTML}{252243}
+        \definecolor{FAAMLightBlue}{HTML}{0ABBEF}
+        \usepackage{eso-pic}
+        \usepackage{pict2e}
+        \newcommand\BackgroundPic{
+        \put(300,-260){
+            \color{FAAMLightBlue}\circle*{900}
+        }
+        \put(300,-260){
+            \color{FAAMDarkBlue}\circle*{760}
+        }
+    }''',
+    # Pretty hacky this - escaping from the sphinx macros,
+    # but it sorta kinda works well enough.
+    'maketitle': '''
+        \\AddToShipoutPicture*{{\\BackgroundPic}}
+        \\begin{{titlepage}}
+            \\color{{FAAMDarkBlue}}
+            \\begin{{flushright}}
+                \\sphinxlogo
+                {{\\sffamily
+                    {{\\Huge \\textbf{{ {project} }}}}
+                    \\par\\vspace{{1cm}}
+                    {{\\itshape\\LARGE\\textbf{{ Release {release} }}}}
+                    \\par\\vspace{{3cm}}
+                    {{\\LARGE \\textbf{{{author}}}}}
+                    \\par\\vspace{{3cm}}
+                    {{\\Large \\textbf{{{date}}}}}
+                 }}
+             \\end{{flushright}}
+        \\end{{titlepage}}
+        '''.format(
+            project=project,
+            release=release,
+            author=author,
+            date=datetime.date.today().strftime('%B %-d, %Y')
+        )
+}
+latex_logo = 'static/faam.png'
