f3159b5cf980cd80b89867dbfe3be6fa4b0c51a5
Dave Sproson <dave.sproson@faam.ac.uk>
2026-01-13 17:37:26 +0000

    Add new default error message.

--- ppodd/exceptions/messages.py
+++ ppodd/exceptions/messages.py
@@ -1,6 +1,13 @@
 EM_CANNOT_INIT_MODULE = (
-    'Cannot initialise module {module_name} as one or more of the constants '
-    '({constants}) are not defined on the controlling Dataset. These are '
-    'required at initialization time. This operation cannot be deferred. '
-    'ensure these constants are set if this module is required.'
+    "Cannot initialise module {module_name} as one or more of the constants "
+    "({constants}) are not defined on the controlling Dataset. These are "
+    "required at initialization time. This operation cannot be deferred. "
+    "ensure these constants are set if this module is required."
+)
+
+EM_CANNOT_RUN_MODULE_CONSTANTS = (
+    "Cannot run module {module_name} as one or more of the constants "
+    "({constants}) are not defined on the controlling Dataset. These "
+    "are required at run time. ensure these constants are set if this "
+    "module is required."
 )
