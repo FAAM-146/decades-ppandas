c94c879ec7131fd73f7f1ee78213be5c1d679ca1
Dave Sproson <dave.sproson@faam.ac.uk>
2026-01-20 17:59:40 +0000

    Refactor load_plugins function to improve plugin loading mechanism and enhance error handling.

--- ppodd/pod/__init__.py
+++ ppodd/pod/__init__.py
@@ -1,38 +1,47 @@
 import os
 import re
 import importlib
-
-def load_plugins():
-    pysearchre = re.compile('.py$', re.IGNORECASE)
-
-    pluginfiles = filter(
-        pysearchre.search,
-        os.listdir(os.path.dirname(__file__))
-    )
-
-    plugins = map(lambda f: '.' + os.path.splitext(f)[0], pluginfiles)
-
-    importlib.import_module('ppodd.pod')
-
-    modules = []
-    for plugin in plugins:
-        if not plugin.startswith('_'):
-            modules.append(importlib.import_module(plugin, package="ppodd.pod"))
-
-    return modules
+import logging
+from types import ModuleType
 
 
-#def compile_cython():
-#    """
-#    Does a basic cython compile if required.
-#    """
-#    import pyximport
-#    pyximport.install(
-#        setup_args={
-#            'include_dirs': np.get_include()
-#        }
-#    )
-#
-#
+logger = logging.getLogger(__name__)
+
+
+def load_plugins() -> list[ModuleType]:
+    """
+    Find all plugin files in the pod directory or its subdirectories
+    that match the pattern p_*.p, import them, and add them to a list of modules.
+    """
+
+    plugin_modules: list[ModuleType] = []
+    pod_dir = os.path.dirname(__file__)
+    pattern = re.compile(r"^p_.*\.py$")
+
+    # Walk through the pod directory and its subdirectories
+    for root, _, files in os.walk(pod_dir):
+        for file in files:
+            if pattern.match(file):
+                # Construct the module name relative to the pod directory
+                module_path = os.path.splitext(
+                    os.path.relpath(os.path.join(root, file), pod_dir)
+                )[0].replace(os.sep, ".")
+
+                # Import the module
+                full_module_name = f"ppodd.pod.{module_path}"
+                try:
+                    module = importlib.import_module(full_module_name)
+                    logger.debug(f"Imported plugin module: {full_module_name}")
+                    plugin_modules.append(module)
+                except ImportError:
+                    logger.warning(
+                        f"Failed to import plugin module: {full_module_name}"
+                    )
+                except Exception as e:
+                    logger.warning(
+                        f"Error occurred while importing module {full_module_name}: {e}"
+                    )
+    return plugin_modules
+
+
 load_plugins()
-##pp_modules = PPBase.__subclasses__()
