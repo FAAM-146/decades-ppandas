1e02311ccf1a1e16460b67b4608eb039b5699853
Dave Sproson <dave.sproson@faam.ac.uk>
2026-01-21 11:35:28 +0000

    Refactor reader modules: update string formatting and remove unused CCN and FGGA readers.

--- ppodd/readers/__init__.py
+++ ppodd/readers/__init__.py
@@ -1,14 +1,16 @@
 from typing import TYPE_CHECKING, Callable
 
+
 if TYPE_CHECKING:
     from .readers import FileReader
 
-reader_patterns: dict[str, type['FileReader']] = {}
+
+reader_patterns: dict[str, type["FileReader"]] = {}
 
 
 def register(patterns: list[str] | None = None) -> Callable:
     # TODO: This should be a generic decorator
-    def _register(f: type['FileReader']) -> type['FileReader']:
+    def _register(f: type["FileReader"]) -> type["FileReader"]:
         nonlocal patterns
         if patterns is None:
             patterns = []
@@ -20,6 +22,4 @@ def register(patterns: list[str] | None = None) -> Callable:
 
 
 from ppodd.readers.readers import *
-from ppodd.readers.fgga import FGGAReader
 from ppodd.readers.sea.reader import WcmFileReader
-from ppodd.readers.ccn import CCNReader
