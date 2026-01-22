894bc442345ce0f4dce5bca64cf65021c2c77977
Dave Sproson <dave.sproson@faam.ac.uk>
2026-01-22 12:04:28 +0000

    Refactor TecoSO2V2 class for consistency in string formatting and improve code readability.

--- ppodd/pod/p_tei_so2_v2.py
+++ ppodd/pod/p_tei_so2_v2.py
@@ -14,12 +14,13 @@ WOW_FLAG_BUFFER = 10
 ZERO_FLAG_BUFFER = 10
 CAL_FLAG_BUFFER = 220
 
-@register_pp('core')
+
+@register_pp("core")
 class TecoSO2V2(PPBase):
     r"""
     Calculate SO\ :math:`_2` concentration from the TECO 43 instrument.
 
-    Zeros are taken whenever valve states V7 or V8 are 1, and are interpolated back to 1 Hz, 
+    Zeros are taken whenever valve states V7 or V8 are 1, and are interpolated back to 1 Hz,
     assuming a linear drift of the offsets between zeros. SO\ :math:`_2` concentration is
     then given by
 
@@ -36,77 +37,88 @@ class TecoSO2V2(PPBase):
     VALID_AFTER = datetime.date(2021, 9, 1)
 
     inputs = [
-        'CHTSOO_concentration',
-        'CHTSOO_flags',
-        'CHTSOO_pmt_volt',
-        'CHTSOO_MFM',
-        'CHTSOO_MFC3_mass_flow',
-        'CHTSOO_V6',
-        'CHTSOO_V7',
-        'CHTSOO_V8',
-        'WOW_IND',
-        'CHTSOO_SENS'
+        "CHTSOO_concentration",
+        "CHTSOO_flags",
+        "CHTSOO_pmt_volt",
+        "CHTSOO_MFM",
+        "CHTSOO_MFC3_mass_flow",
+        "CHTSOO_V6",
+        "CHTSOO_V7",
+        "CHTSOO_V8",
+        "WOW_IND",
+        "CHTSOO_SENS",
     ]
 
     @staticmethod
     def test():
         return {
-            'CHTSOO_concentration': ('data', _c([
-                _o(10), _z(10), _o(10), _z(10), _o(60)
-            ]), 1),
-            'CHTSOO_flags': ('data', _o(100) * 40000000, 1),
-            'CHTSOO_pmt_volt': ('data', _o(100) * -700, 1),
-            'CHTSOO_MFM': ('data', _o(100) * 1.2, 1),
-            'CHTSOO_MFC3_mass_flow': ('data', _o(100) * 1.9, 1),
-            'CHTSOO_V6': ('data', _c([_z(10), _o(10), _z(10), _o(10), _z(60)]), 1),
-            'CHTSOO_V7': ('data', _c([_z(10), _o(10), _z(10), _o(10), _z(60)]), 1),
-            'CHTSOO_V8': ('data', _z(100), 1),
-            'WOW_IND': ('data', _c([_o(20), _z(70), _o(10)]), 1),
-            'CHTSOO_SENS': ('const', 1),
+            "CHTSOO_concentration": (
+                "data",
+                _c([_o(10), _z(10), _o(10), _z(10), _o(60)]),
+                1,
+            ),
+            "CHTSOO_flags": ("data", _o(100) * 40000000, 1),
+            "CHTSOO_pmt_volt": ("data", _o(100) * -700, 1),
+            "CHTSOO_MFM": ("data", _o(100) * 1.2, 1),
+            "CHTSOO_MFC3_mass_flow": ("data", _o(100) * 1.9, 1),
+            "CHTSOO_V6": ("data", _c([_z(10), _o(10), _z(10), _o(10), _z(60)]), 1),
+            "CHTSOO_V7": ("data", _c([_z(10), _o(10), _z(10), _o(10), _z(60)]), 1),
+            "CHTSOO_V8": ("data", _z(100), 1),
+            "WOW_IND": ("data", _c([_o(20), _z(70), _o(10)]), 1),
+            "CHTSOO_SENS": ("const", 1),
             # Optional calibration info...
-            'CHTSOO_CALINFO_DATE': ('const', DocAttribute(
-                value=datetime.date(2000, 1, 1), doc_value=OptionalDerivedString
-            )),
-            'CHTSOO_CALINFO_INFO': ('const', DocAttribute(
-                value='Calibrated in a lab', doc_value=OptionalDerivedString
-            )),
-            'CHTSOO_CALINFO_URL': ('const', DocAttribute(
-                value='https://some.url', doc_value=OptionalDerivedString
-            ))
+            "CHTSOO_CALINFO_DATE": (
+                "const",
+                DocAttribute(
+                    value=datetime.date(2000, 1, 1), doc_value=OptionalDerivedString
+                ),
+            ),
+            "CHTSOO_CALINFO_INFO": (
+                "const",
+                DocAttribute(
+                    value="Calibrated in a lab", doc_value=OptionalDerivedString
+                ),
+            ),
+            "CHTSOO_CALINFO_URL": (
+                "const",
+                DocAttribute(value="https://some.url", doc_value=OptionalDerivedString),
+            ),
         }
 
     def declare_outputs(self):
         self.declare(
-            'SO2_TECO',
-            units='ppb',
+            "SO2_TECO",
+            units="ppb",
             frequency=1,
-            long_name='Mole fraction of Sulphur Dioxide in air from TECO 43 instrument',
-            standard_name='mole_fraction_of_sulfur_dioxide_in_air',
-            instrument_manufacturer='Thermo Fisher Scientific, Inc.',
-            instrument_model='43i Trace Level-Enhanced pulsed fluorescence SO2 spectrometer',
-            instrument_serial_number='1505564557',
-            calibration_information=self.dataset.lazy['CHTSOO_CALINFO_INFO'],
-            calibration_date=self.dataset.lazy['CHTSOO_CALINFO_DATE'],
-            calibration_url=self.dataset.lazy['CHTSOO_CALINFO_URL']
+            long_name="Mole fraction of Sulphur Dioxide in air from TECO 43 instrument",
+            standard_name="mole_fraction_of_sulfur_dioxide_in_air",
+            instrument_manufacturer="Thermo Fisher Scientific, Inc.",
+            instrument_model="43i Trace Level-Enhanced pulsed fluorescence SO2 spectrometer",
+            instrument_serial_number="1505564557",
+            instrument_description=self.dataset.lazy["CHTSOO_DESCRIPTION"],
+            calibration_information=self.dataset.lazy["CHTSOO_CALINFO_INFO"],
+            calibration_date=self.dataset.lazy["CHTSOO_CALINFO_DATE"],
+            calibration_url=self.dataset.lazy["CHTSOO_CALINFO_URL"],
         )
 
         self.declare(
-            'SO2_TECO_ZERO',
-            units='ppb',
+            "SO2_TECO_ZERO",
+            units="ppb",
             frequency=1,
-            long_name='TECO 43 SO2 interpolated zero',
-            instrument_manufacturer='Thermo Fisher Scientific, Inc.',
-            instrument_model='43i Trace Level-Enhanced pulsed fluorescence SO2 spectrometer',
-            instrument_serial_number='1505564557',
-            calibration_information=self.dataset.lazy['CHTSOO_CALINFO_INFO'],
-            calibration_date=self.dataset.lazy['CHTSOO_CALINFO_DATE'],
-            calibration_url=self.dataset.lazy['CHTSOO_CALINFO_URL'],
-            write=False
+            long_name="TECO 43 SO2 interpolated zero",
+            instrument_manufacturer="Thermo Fisher Scientific, Inc.",
+            instrument_model="43i Trace Level-Enhanced pulsed fluorescence SO2 spectrometer",
+            instrument_serial_number="1505564557",
+            instrument_description=self.dataset.lazy["CHTSOO_DESCRIPTION"],
+            calibration_information=self.dataset.lazy["CHTSOO_CALINFO_INFO"],
+            calibration_date=self.dataset.lazy["CHTSOO_CALINFO_DATE"],
+            calibration_url=self.dataset.lazy["CHTSOO_CALINFO_URL"],
+            write=False,
         )
 
     def get_wow_flag(self):
         d = self.d
-        wow = self.d['WOW_IND'] == 1
+        wow = self.d["WOW_IND"] == 1
 
         # Get the takeoff time - emulate this if we're in test mode.
         to = self.dataset.takeoff_time
@@ -116,41 +128,41 @@ class TecoSO2V2(PPBase):
         wow.loc[
             [to + datetime.timedelta(seconds=i) for i in range(WOW_FLAG_BUFFER)]
         ] = True
-        d['wow_flag'] = wow
+        d["wow_flag"] = wow
 
     def get_status_flag(self):
-        self.d['status_flag'] = 0
-        flags = self.d['CHTSOO_flags']
-        volts = self.d['CHTSOO_pmt_volt']
-        
-        self.d.loc[flags != 40000000, 'status_flag'] = 1
-        self.d.loc[volts > -690, 'status_flag'] = 1
+        self.d["status_flag"] = 0
+        flags = self.d["CHTSOO_flags"]
+        volts = self.d["CHTSOO_pmt_volt"]
+
+        self.d.loc[flags != 40000000, "status_flag"] = 1
+        self.d.loc[volts > -690, "status_flag"] = 1
 
     def get_mfm_flag(self):
-        self.d['mfm_flag'] = 0
-        self.d.loc[self.d['CHTSOO_MFM'] < 1.2, 'mfm_flag'] = 1
+        self.d["mfm_flag"] = 0
+        self.d.loc[self.d["CHTSOO_MFM"] < 1.2, "mfm_flag"] = 1
 
     def get_flow_flag(self):
-        self.d['flow_flag'] = 0
-        self.d.loc[self.d['CHTSOO_MFC3_mass_flow'] < 1.9, 'flow_flag'] = 1
+        self.d["flow_flag"] = 0
+        self.d.loc[self.d["CHTSOO_MFC3_mass_flow"] < 1.9, "flow_flag"] = 1
 
     def get_cal_or_zero_flag(self):
-        in_cal = self.d['CHTSOO_V8'] == 1
-        in_zero = (self.d['CHTSOO_V6'] == 1) | (self.d['CHTSOO_V7'] == 1)
+        in_cal = self.d["CHTSOO_V8"] == 1
+        in_zero = (self.d["CHTSOO_V6"] == 1) | (self.d["CHTSOO_V7"] == 1)
 
         leaving_zero = (in_zero - in_zero.shift()) == -1
         leaving_zero = leaving_zero[leaving_zero]
-        
+
         for i in leaving_zero.index:
-            in_zero.loc[i:i+datetime.timedelta(seconds=ZERO_FLAG_BUFFER)] = True
-            
+            in_zero.loc[i : i + datetime.timedelta(seconds=ZERO_FLAG_BUFFER)] = True
+
         leaving_cal = (in_cal - in_cal.shift()) == -1
         leaving_cal = leaving_cal[leaving_cal]
 
         for i in leaving_cal.index:
-            in_cal.loc[i:i+datetime.timedelta(seconds=CAL_FLAG_BUFFER)] = True
+            in_cal.loc[i : i + datetime.timedelta(seconds=CAL_FLAG_BUFFER)] = True
 
-        self.d['zero_cal_flag'] = in_zero | in_cal
+        self.d["zero_cal_flag"] = in_zero | in_cal
 
     def flag(self, var):
         self.get_wow_flag()
@@ -158,64 +170,73 @@ class TecoSO2V2(PPBase):
         self.get_mfm_flag()
         self.get_flow_flag()
         self.get_cal_or_zero_flag()
-        
-        flag_info = {
-            'wow_flag': (
-                'aircraft_on_ground',
-                (f'The aircraft is on the ground, as indicated by the weight on '
-                 f'wheels flag. An additional {WOW_FLAG_BUFFER} seconds are added'
-                 f'after take-off to allow for instrument flushing.')
-            ),
-            'status_flag': (
-                'instrument flagged',
-                ('The instrument is reporting an alarm for one of the following: '
-                 'internal or fluorescence cell temperatures, lamp intensity or '
-                  'voltage, PMT voltage or fluorscence cell pressure.')
-            ),
-            'mfm_flag': (
-                'inlet flow out of range',
-                'The inlet sample overflow is out of range.'
-            ),
-            'flow_flag': (
-                'sample flow out of range',
-                'The spectrometer sample mass flow rate is out of range.'
-            ),
-            'zero_cal_flag': (
-                'instrument in cal mode',
-                'The instrument is being calibrated for either span or zero' 
-            )
 
+        flag_info = {
+            "wow_flag": (
+                "aircraft_on_ground",
+                (
+                    f"The aircraft is on the ground, as indicated by the weight on "
+                    f"wheels flag. An additional {WOW_FLAG_BUFFER} seconds are added"
+                    f"after take-off to allow for instrument flushing."
+                ),
+            ),
+            "status_flag": (
+                "instrument flagged",
+                (
+                    "The instrument is reporting an alarm for one of the following: "
+                    "internal or fluorescence cell temperatures, lamp intensity or "
+                    "voltage, PMT voltage or fluorscence cell pressure."
+                ),
+            ),
+            "mfm_flag": (
+                "inlet flow out of range",
+                "The inlet sample overflow is out of range.",
+            ),
+            "flow_flag": (
+                "sample flow out of range",
+                "The spectrometer sample mass flow rate is out of range.",
+            ),
+            "zero_cal_flag": (
+                "instrument in cal mode",
+                "The instrument is being calibrated for either span or zero",
+            ),
         }
 
         for col, (short, long) in flag_info.items():
             var.flag.add_mask(self.d[col], short, long)
 
     def process(self):
-        
         self.get_dataframe()
-        sens = self.dataset['CHTSOO_SENS']
+        sens = self.dataset["CHTSOO_SENS"]
 
         # The zero flag is
         # - when ev7 is active (in a zero)
         # - ev8 is inactive (not in a cal)
         # - ev8 has been inactive for at least CAL_FLAG_BUFFER secs
-        ev8 = self.d['CHTSOO_V8']
+        ev8 = self.d["CHTSOO_V8"]
         ev8_buffered = ev8.copy()
 
-        for i in ev8[ev8.diff()==-1].index:
-            ev8_buffered.loc[i:i+datetime.timedelta(seconds=CAL_FLAG_BUFFER)] = 1
+        for i in ev8[ev8.diff() == -1].index:
+            ev8_buffered.loc[i : i + datetime.timedelta(seconds=CAL_FLAG_BUFFER)] = 1
 
-        self.d['zero_flag'] = (self.d['CHTSOO_V7'] == 1) & (~(ev8_buffered==1))
+        self.d["zero_flag"] = (self.d["CHTSOO_V7"] == 1) & (~(ev8_buffered == 1))
 
-        flagged_avg(self.d, 'zero_flag', 'CHTSOO_concentration', out_name='zero',
-                    interp=True, skip_start=CAL_FLUSH_START, skip_end=CAL_FLUSH_END)
+        flagged_avg(
+            self.d,
+            "zero_flag",
+            "CHTSOO_concentration",
+            out_name="zero",
+            interp=True,
+            skip_start=CAL_FLUSH_START,
+            skip_end=CAL_FLUSH_END,
+        )
 
-        self.d['zero'] = self.d['zero'].ffill()
+        self.d["zero"] = self.d["zero"].ffill()
 
-        conc = (self.d['CHTSOO_concentration'] - self.d['zero']) / sens
+        conc = (self.d["CHTSOO_concentration"] - self.d["zero"]) / sens
 
-        so2 = DecadesVariable(conc, flag=DecadesBitmaskFlag, name='SO2_TECO')
-        zero = DecadesVariable(self.d['zero'], flag=None, name='SO2_TECO_ZERO')
+        so2 = DecadesVariable(conc, flag=DecadesBitmaskFlag, name="SO2_TECO")
+        zero = DecadesVariable(self.d["zero"], flag=None, name="SO2_TECO_ZERO")
 
         self.flag(so2)
 
